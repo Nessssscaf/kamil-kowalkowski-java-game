0d63adddff4d765aa443d26a2909c232009530a6
Kamil Kowalkowski <https://github.com/Nessssscaf/kamil-kowalkowski-kodilla-java.git>
2022-05-10 21:27:49 +0200

    first commit

---
+++ src/main/java/com/game/javagame/JavaGameApplication.java
@@ -0,0 +1,13 @@
+package com.game.javagame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/game/javagame/JavaGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.javagame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
